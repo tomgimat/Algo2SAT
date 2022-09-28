ec3a4e2f3f43a822fcae1491365a2d05f8076d5b
Tom Gimat <tom6gimat@gmail.com>
2022-09-28 16:57:58 +0200

    pas fini mais tb

--- src/TP1/Graph.java
+++ src/TP1/Graph.java
@@ -1,6 +1,7 @@
 package TP1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Graph<Label> {
@@ -39,13 +40,33 @@ public class Graph<Label> {
 
     public String toString() {
         String result = new String("");
-        result.concat(cardinal + "\n");
+        result+=(cardinal + "\n");
         for (int i = 0; i < cardinal; i++) {
             for (Edge e : incidency.get(i)) {
-                result.concat(e.source + " " + e.destination + " "
+                result+=(e.source + " " + e.destination + " "
                         + e.label.toString() + "\n");
             }
         }
         return result;
+    }
+
+    public ArrayList<Integer> parcours_en_profondeur(){
+        ArrayList<Integer> datesFin = new ArrayList<>();
+        HashMap<LinkedList<Edge>, Boolean> marked = new HashMap<>();
+        incidency.forEach(i->{
+            if(!marked.get(i)){
+                datesFin.add(explore(i, 0, marked));
+            }
+        });
+        return datesFin;
+    }
+    public int explore(LinkedList<Edge> sommet, int date, HashMap<LinkedList<Edge>, Boolean> marked){
+        marked.put(sommet, true);
+        sommet.forEach(s->{
+            if(!marked.get(s.destination)){
+                explore(sommet,date+1, marked);
+            }
+        });
+        return date;
     }
 }
--- src/TP1/TP1.java
+++ src/TP1/TP1.java
@@ -10,40 +10,60 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TP1 {
     public static ArrayList<ArrayList<Integer>> clauses;
-    public static int nombre_de_variables, nombre_de_clauses;
+    public static int nombre_de_sommets;
     public static void main(String[] args) throws IOException {
+
         try {
             read();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        for(ArrayList<Integer> list : clauses){
-            System.out.println(list.get(0) + " " + list.get(1) );
-        }
-        Graph<String> graph = new Graph<String>(nombre_de_variables*2);
     }
     public static void read()
             throws Exception {
         Path path = Paths.get("src/TP1/formule.txt");
 
-        for(String line: Files.readAllLines(path)){
-            if (line.startsWith("p")) {
-                nombre_de_variables = (int) line.charAt(6);
-                nombre_de_clauses = (int) line.charAt(8);
-                clauses = new ArrayList<>(nombre_de_clauses);
-            } else if(!line.startsWith("c") && line.endsWith("0")) {
-                String[] line_splitted = line.substring(0, line.length()-1).split(" ");
-                clauses.add(new ArrayList<Integer>(nombre_de_variables));
-                for(String str : line_splitted){
-                    clauses.get(clauses.size()-1).add(Integer.parseInt(str));
-                }
+        Scanner sc = new Scanner(path);
 
-            } else {
-                throw new Exception("ddddd");
-            }
+        nombre_de_sommets = sc.nextInt() * 2;
+        System.out.println(nombre_de_sommets);
+
+        int index = 0;
+        Graph<Integer> graph = new Graph<Integer>(nombre_de_sommets);
+        while(sc.hasNextLine()){
+            int litteral1 = sc.nextInt();
+            System.out.println(litteral1);
+            int litteral2 = sc.nextInt();
+            sc.nextInt();
+            System.out.println(litteral2);
+            graph.addArc(litteralToVertex(-litteral1), litteralToVertex(litteral2), index++);
+            graph.addArc(litteralToVertex(-litteral2), litteralToVertex(litteral1), index++);
         }
+        System.out.println(graph.toString());
+
+//        for(String line: Files.readAllLines(path)){
+//            if (line.startsWith("p")) {
+//                nombre_de_variables = (int) line.charAt(6);
+//                nombre_de_clauses = (int) line.charAt(8);
+//                clauses = new ArrayList<>(nombre_de_clauses);
+//            } else if(!line.startsWith("c") && line.endsWith("0")) {
+//                String[] line_splitted = line.substring(0, line.length()-1).split(" ");
+//                clauses.add(new ArrayList<Integer>(nombre_de_variables));
+//                for(String str : line_splitted){
+//                    clauses.get(clauses.size()-1).add(Integer.parseInt(str));
+//                }
+//
+//            } else {
+//                throw new Exception("ddddd");
+//            }
+//        }
+    }
+
+    public static int litteralToVertex(int litteral){
+        return litteral>0 ? (2*litteral-2) : (-2*litteral-1);
     }
 }
